5c64b99de6b386f74abebe3fdb3d877e8dd3dfa3
Deyvid Yury <deyvidyury@yahoo.com.br>
2017-07-28 16:29:18 -0300

    Credentials

--- src/app/firebase.credentials.ts
+++ src/app/firebase.credentials.ts
@@ -1,8 +1,3 @@
 export const FIREBASE_CREDENTIALS = {
-	apiKey: "AIzaSyB1V6fJxCd-g73yXU67DT9MwWauUWoGS7Q",
-    authDomain: "fir-crud-f562a.firebaseapp.com",
-    databaseURL: "https://fir-crud-f562a.firebaseio.com",
-    projectId: "fir-crud-f562a",
-    storageBucket: "fir-crud-f562a.appspot.com",
-    messagingSenderId: "168532614763"
+	// use your credentials from firebase
 };
